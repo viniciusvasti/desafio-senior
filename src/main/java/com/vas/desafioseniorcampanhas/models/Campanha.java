fe77e8c5129211d9e65cf7548e118716319ff898
Vinícius A. dos Santos <vinicius.vas.ti@gmail.com>
2019-06-11 23:49:57 -0300

    defined mongodb port and database name

--- src/main/java/com/vas/desafioseniorcampanhas/models/Campanha.java
+++ src/main/java/com/vas/desafioseniorcampanhas/models/Campanha.java
@@ -2,13 +2,19 @@ package com.vas.desafioseniorcampanhas.models;
 
 import java.sql.Date;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Document
 public class Campanha {
 	
+	@Id
+	private String id;
 	private String nome;
 	private long idTimeDoCoracao;
 	private Date dataVigencia;
