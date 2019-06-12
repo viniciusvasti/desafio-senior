882d2dd869b8ae94e72ed9867b609d12dfcd710e
Vinícius A. dos Santos <vinicius.vas.ti@gmail.com>
2019-06-11 21:33:34 -0300

    Creating project

---
+++ src/main/java/com/vas/desafioseniorcampanhas/DesafioSeniorCampanhasApplication.java
@@ -0,0 +1,13 @@
+package com.vas.desafioseniorcampanhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSeniorCampanhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSeniorCampanhasApplication.class, args);
+	}
+
+}
